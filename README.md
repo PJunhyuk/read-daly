0611cc903c0a1d621525adb8989e5ae8082b308a
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-03-02 15:16:52 +0900

    Update README and Add sample

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# read-daly
+# read-daly  
+Read and Show [DALY Dataset](http://thoth.inrialpes.fr/daly/)  
+> DALY - Daily Action Localization in Youtube videos - is intended for research in action recognition. It is suitable to train and test temporal/spatial localization algorithms.
+
+## Demo  
+<img src="/samples/rg3Mr6e1KMo_sample.gif" width="600">
+
+## Usage
+
+#### Basic
+```
+> python read-daly.py
+```
+
+###### Arguments  
+> -n, --videoNumber = Get specific number of video  
+> -r, --randomVideo = Get random Video  
+> -s, --saveVideo = Save result as avi format  
+
+###### Example
+```
+> python read-daly.py -r 1 -s 1
+```
+> Get random video, and save result as avi format
+
+```
+> python read-daly.py -n 10
+```
+> Get 10th video, and just show it's result
+
+## Dependencies
+
+Install
+- Python 3.6.4
+- numpy 1.14.0
+- opencv-python 3.4.0.12
+- ...
+
+## Reference
+
+### Citation
+    @article{DBLP:journals/corr/WeinzaepfelMS16,
+      author    = {Philippe Weinzaepfel and
+                   Xavier Martin and
+                   Cordelia Schmid},
+      title     = {Towards Weakly-Supervised Action Localization},
+      journal   = {CoRR},
+      volume    = {abs/1605.05197},
+      year      = {2016}
+    }
---
+++ read-daly.py
@@ -0,0 +1,113 @@
+## for daly dataset
+import pickle
+
+## for opencv-python
+import numpy as np
+import cv2
+
+## for file list parsing
+import glob
+
+import argparse as ap
+
+import random
+
+video_name_list = glob.glob("D:/workspace-dataset/DALY/download_videos/videos/*.mp4")
+
+parser = ap.ArgumentParser()
+parser.add_argument('-n', "--videoNumber", help="Get specific number of video")
+parser.add_argument('-r', "--randomVideo", help="Get random video")
+parser.add_argument('-s', "--saveVideo", help="Save result as avi format")
+
+args = vars(parser.parse_args())
+
+if args["videoNumber"] is not None:
+    video_number = int(args["videoNumber"])
+else:
+    video_number = 0
+
+if args["randomVideo"] is not None:
+    video_number = random.randrange(0, len(video_name_list))
+
+video_id_list = []
+
+for i in range(0, len(video_name_list)):
+    video_name_len = len(video_name_list[i])
+    video_id_list.append(video_name_list[i][video_name_len-15:video_name_len])
+
+## sample video
+video_id = video_id_list[video_number]
+video_name = video_name_list[video_number]
+
+## open daly dataset from the pickle file
+with open("D:\workspace-dataset\DALY\daly1.1.0.pkl", "rb") as f:
+    daly = pickle.load(f, encoding='latin1')
+
+## play sample video
+cap = cv2.VideoCapture(video_name)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print(daly['annot'][video_id]['annot'].keys())
+action_list = list(daly['annot'][video_id]['annot'].keys())
+print(action_list)
+action = daly['annot'][video_id]['annot'][action_list[0]]
+
+action_frame_list = []
+for i in range(0, len(action)):
+    print(int(action[i]['beginTime'] * fps))
+    print(int(action[i]['endTime'] * fps))
+    action_frame_list.append([int(action[i]['beginTime'] * fps), int(action[i]['endTime'] * fps)])
+print(action_frame_list)
+
+if action_frame_list[0][0] > 200:
+    start_frame = action_frame_list[0][0] - 100
+else:
+    start_frame = 0
+
+cap.set(1, start_frame);
+
+frame_number = start_frame
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#### save video
+if args["saveVideo"] is not None:
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    output_video_name = video_id.split('.')[0] + '_output.avi'
+    out = cv2.VideoWriter(output_video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (int(cap.get(3)), int(cap.get(4))))
+
+while(cap.isOpened()):
+    frame_number = frame_number + 1
+    print(str(frame_number))
+
+    ret, frame = cap.read()
+
+    if ret == True:
+        for j in range(0, len(action)):
+            if frame_number >= action_frame_list[j][0] and frame_number <= action_frame_list[j][1]:
+                print('YEAH!')
+                xmin = action[j]['keyframes'][0]['boundingBox'][0][0]
+                ymin = action[j]['keyframes'][0]['boundingBox'][0][1]
+                xmax = action[j]['keyframes'][0]['boundingBox'][0][2]
+                ymax = action[j]['keyframes'][0]['boundingBox'][0][3]
+                frame = cv2.rectangle(frame, (int(width * xmin), int(height * ymin)), (int(width * xmax), int(height * ymax)), (255,0,0), 5)
+                cv2.putText(frame, action_list[0], (int(width * xmin) + 10, int(height * ymin) + 30), font, 1, (255,0,0), 3, cv2.LINE_AA)
+
+        if args["saveVideo"] is not None:
+            out.write(frame)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+
+if args["saveVideo"] is not None:
+    out.release()
+
+cv2.destroyAllWindows()
